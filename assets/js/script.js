fe00cc95d7498c9048ee69da6c7755f8c8dbdcaa
noemi-droit llampa <noemillampa50@gmail.com>
2024-02-06 05:13:06 +0000

    Add changes to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,38 +10,51 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("rock");
 
 });
 
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
 
-function runGame(){
+    // Creates two random numbers 
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+    
+    
+}
+
+
+
+function displayRockOption(){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
+    
+}
+
+function displayPaperOption(){
+
+    
 
 }
 
-function checkAnswer(){
+function displayScissorOption(){
+   
+    }
 
-}
-
-function claculateCorrectAnswer(){
-
-}
-
-function incrementScore(){
-
-}
-
-function displayRockQuestion(){
-
-}
-
-function displayPaperQuestion(){
-
-}
-
-function displayScissorQuestion(){
-
-}
